f508ecc4df81f2793d6e3a415b274112bdeb701e
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 12:26:47 -0300

    Update 6-iteraciones.js

--- ExamenViejo/codigoFuenteParcial/6-iteraciones.js
+++ ExamenViejo/codigoFuenteParcial/6-iteraciones.js
@@ -2,7 +2,31 @@
 function Mostrar()
 {
 	
-	
+	var maximo;
+    var minimo;
+    var diaMax;
+    var diaMin;
+    var flag = 0;
+
+   for (vari=1; i <=7 ; i++)
+   {
+       while ( importe <=0 || isNan (importe))
+       {
+           importe = parseInt(prompt("Error, Ingrese el importe"));
+       }
+       if ( importe > maximo || flag ==0)
+       {
+           maximo = importe;
+           diaMax = i;
+       } 
+       if (importe <minimo || flag ==o)
+       {
+           minimo = importe;
+           flag = i;
+       }
+   }
+
+	alert("Mayor importe $" + maximo + "el día " + diaMax + "\nMenor importe $" + minimo + "el día " );
 
 	
 }

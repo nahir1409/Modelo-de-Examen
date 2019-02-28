1fa5c878ff778fe5c0772c0c497bc4393d979ced
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 11:13:26 -0300

    Update 2-entradaSalida.js

--- ExamenViejo/codigoFuenteParcial/2-entradaSalida.js
+++ ExamenViejo/codigoFuenteParcial/2-entradaSalida.js
@@ -1,7 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
-{
+{   
 	
+    var impote;
+    var iva;
+    var importeFinal;
+
+    importe = parseInt(prompt("Ingrese el importe"));
+
+    iva = importe * 21/100;
+    importeFinal= importe + iva;
+
+    document.getElementById("importe").value=importeFinal;
+    
 
 }
 

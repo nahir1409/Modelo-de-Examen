40b0f761789ec15fc48dba63b916e4df37b61995
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 11:38:45 -0300

    Update 5-switch.js

--- ExamenViejo/codigoFuenteParcial/5-switch.js
+++ ExamenViejo/codigoFuenteParcial/5-switch.js
@@ -1,7 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+	var mes;
+    mes = prompt("Ingrese un mes: ");
+
+    switch (mes) {
+    case "Enero":
+    case "Febrero":
+        alert("Veranito!!!");
+        break;
+    default:
+        alert("No extraño para nada Enero y Febrero. ¡Aguante el frío PAPÁ!");
+        break;
+   //volver a hacer, está mal
 	
 	
 }

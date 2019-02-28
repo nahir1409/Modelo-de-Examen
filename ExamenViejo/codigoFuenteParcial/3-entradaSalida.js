849159f85bed6471c08bd9edc92a8c267aafcc3a
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 11:16:47 -0300

    Update 3-entradaSalida.js

--- ExamenViejo/codigoFuenteParcial/3-entradaSalida.js
+++ ExamenViejo/codigoFuenteParcial/3-entradaSalida.js
@@ -1,7 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	
+    var largo;
+    var ancho;
+    var perimetro;
+    var alambre;
+
+    
+    largo = parseInt(document.getElementById("largo").value);
+    ancho = parseInt(document.getElementById("ancho").value);
+
+    perimetro = (largo + ancho) * 2;
+
+    alambre = perimetro * 6;
+
+
+    alert("Se necesitan comprar: " + alambre + " metros de alambre");
 	
 }
 
